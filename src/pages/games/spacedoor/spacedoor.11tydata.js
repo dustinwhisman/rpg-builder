3922c07515f967e9266267460075f93f58efcdc0
Dustin Whisman <whisman.dustin@gmail.com>
2022-01-16 10:19:45 -0700

    feat: build out a list of status effects

--- src/pages/games/spacedoor/spacedoor.11tydata.js
+++ src/pages/games/spacedoor/spacedoor.11tydata.js
@@ -50,6 +50,79 @@ const toDice = [
   'd20',
 ];
 
+const statusEffects = [
+  {
+    name: 'Asleep',
+    description: 'Affected targets are unable to act in combat. In place of their Action, the target can make a Sharp saving throw to try to wake up. If they succeed, they wake up and can use their Bonus Action and Reaction.',
+    savingThrow: 'Sharp',
+  },
+  {
+    name: 'Stunned',
+    description: 'Affected targets are unable to act in combat. In place of their Action, the target can make a Tough saving throw to try to snap out of it. If they succeed, they can use their Bonus Action and Reaction.',
+    savingThrow: 'Tough',
+  },
+  {
+    name: 'Confused',
+    description: 'Affected targets will randomly attack anybody involved in combat, including themselves. On their turn, the target must roll a d4 to determine who is attacked, with 1 being themselves, and the other numbers being the next closest combatants. At the end of their turn, the Confused target can make a Sharp saving throw to end the effect.',
+    savingThrow: 'Sharp',
+  },
+  {
+    name: 'Frightened',
+    description: 'Affected targets must use their Movement to get away from whoever or whatever inflicted the effect. At the end of their turn, the target can make a Cool saving throw to try to end the effect.',
+    savingThrow: 'Cool',
+  },
+  {
+    name: 'Slow',
+    description: 'Affected targets can use either their Movement or their Action, but not both, and they cannot use their Bonus Action or Reaction. At the end of their turn, the target can make a Cool saving throw to try to end the effect.',
+    savingThrow: 'Cool',
+  },
+  {
+    name: 'Armor Break',
+    description: 'Affected targets have their AC halved (rounded down). At the end of their turn, they can make a Cool saving throw to try to end the effect.',
+    savingThrow: 'Cool',
+  },
+  {
+    name: 'Power Break',
+    description: 'Affected targets deal half damage (rounded down). At the end of their turn, they can make a Tough saving throw to try to end the effect.',
+    savingThrow: 'Tough',
+  },
+  {
+    name: 'Mental Break',
+    description: 'Affected targets have their core stat DCs halved (rounded down). At the end of their turn, they can make a Sharp saving throw to try to end the effect.',
+    savingThrow: 'Sharp',
+  },
+  {
+    name: 'Berserk',
+    description: 'Affected targets must use their Action to attack the nearest enemy on their turn, and they may only use offensive Bonus Actions and Reactions. At the end of their turn, they can make a Sharp saving throw to try to end the effect.',
+    savingThrow: 'Sharp',
+  },
+  {
+    name: 'Blinded',
+    description: 'Affected targets are unable to see. Any rolls that require precision targeting must be made with Disadvantage, and any offensive rolls made against the player will be made with Advantage. At the end of their turn, the target can make a Sharp saving throw to try to end the effect.',
+    savingThrow: 'Sharp',
+  },
+  {
+    name: 'Silenced',
+    description: 'Affected targets cannot speak, and they lose the use of their Bonus Action. At the end of their turn, they can make a Cool saving throw to try to end the effect.',
+    savingThrow: 'Cool',
+  },
+  {
+    name: 'Restrained',
+    description: 'Affected targets cannot use their Movement or Reaction. They can use their Action to make a Tough saving throw to break free, at which point they can use their Movement and Reaction, or they can choose to stay put and use their Action and Bonus Action for something else.',
+    savingThrow: 'Tough',
+  },
+  {
+    name: 'Poisoned',
+    description: 'Affected targets take poison damage at the end of their turn, at which point they can make a Tough saving throw to try to end the effect.',
+    savingThrow: 'Tough',
+  },
+  {
+    name: 'Bleeding',
+    description: 'Affected targets take bleed damage at the end of their turn, at which point they can make a Cool saving throw to try to end the effect.',
+    savingThrow: 'Cool',
+  },
+];
+
 const statUpgrades = Object.keys(stats).reduce((upgradeList, key) => {
   const list = [];
 
@@ -131,6 +204,7 @@ const healthUpgrades = [
 module.exports = {
   stats,
   baseDice,
+  statusEffects,
   statUpgrades,
   skillUpgrades,
   healthUpgrades,
